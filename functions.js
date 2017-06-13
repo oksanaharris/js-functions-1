19231f3ff2c607b30c6b4bfbb83e552328f5b7ea
Oksana Harr <oksana.harris@gmail.com>
2017-06-12 20:14:37 -1000

    complete

--- functions.js
+++ functions.js
@@ -4,6 +4,12 @@
  * @return {string} the number as a string
  */
 
+ function numberToString (num){
+  return num.toString();
+ }
+
+// console.log(numberToString(2));
+
 
 /**
  * Adds one to a given number.
@@ -11,6 +17,9 @@
  * @return {number}
  */
 
+function increase(num){
+  return num + 1;
+}
 
 /**
  * Subtracts one from a given number.
@@ -18,6 +27,9 @@
  * @return {number}
  */
 
+function decrease(num){
+  return num - 1;
+}
 
 /**
  * Adds two numbers.
@@ -26,6 +38,9 @@
  * @return {number} the sum
  */
 
+function add (num1, num2){
+  return num1+num2;
+}
 
 /**
  * Subtracts the second number from the first.
@@ -34,6 +49,9 @@
  * @return {number} the difference
  */
 
+function subtract (num1, num2){
+  return num1-num2;
+}
 
 /**
  * Multiplies two numbers.
@@ -42,6 +60,9 @@
  * @return {number} the product
  */
 
+function multiply(num1, num2){
+  return num1 * num2;
+}
 
 /**
  * Divides the first number by the second.
@@ -50,12 +71,20 @@
  * @return {number} the quotient
  */
 
+ function divide (num1, num2){
+  return num1/num2;
+ }
+
 
 /**
  * Multiplies a number by itself.
  * @param {number} x, number to be squared
  * @return {number} squared
  */
+
+function square (num){
+  return num*num;
+}
 
 
 /**
@@ -67,6 +96,30 @@
  * @return {number} the result
  */
 
+function calculate(op, num1, num2){
+  var result;
+  switch (op){
+    case 'add': 
+      result = num1 + num2;
+      console.log(num1 + ' + ' + num2 + ' = ' +  result);
+      break;
+    case 'subtract':
+      result = num1 - num2;
+      console.log(num1 + ' - ' + num2 + ' = ' +  result);
+      break;
+    case 'multiply':
+      result = num1 * num2;
+      console.log(num1 + ' * ' + num2 + ' = ' +  result);
+      break;
+    case 'divide':
+      result = num1 / num2;
+      console.log(num1 + ' / ' + num2 + ' = ' +  result);
+      break;
+    default: 
+      console.log("Insert a valid operator");
+  }
+  return result;
+}
 
 /**
  * Returns true if `a` is greater than `b`.
@@ -74,6 +127,17 @@
  * @param {number} b
  * @return {boolean} `a` is larger than `b`
  */
+
+ function isGreaterThan (x, y){
+  if (x > y) {
+    return true;
+  }
+  else {
+    return false;
+  }
+ }
+
+
 
 
 /**
@@ -83,6 +147,14 @@
  * @return {boolean} `a` is smaller than `b`
  */
 
+ function isLessThan (x, y){
+  if (x < y) {
+    return true;
+  }
+  else {
+    return false;
+  }
+ }
 
 /**
  * Returns true if `a` and `b` are equal.
@@ -90,6 +162,15 @@
  * @param {number} b
  * @return {boolean} the numbers are equal
  */
+
+ function areEqual (x, y){
+  if (x === y) {
+    return true;
+  }
+  else {
+    return false;
+  }
+ }
 
 
 /**
@@ -99,6 +180,16 @@
  * @return {number} the smallest number
  */
 
+function minimum (num1, num2){
+  if (num1 < num2){
+    return num1;
+  }
+  else {
+    return num2;
+  }
+}
+
+
 
 /**
  * Returns the largest value of two numbers.
@@ -107,12 +198,29 @@
  * @return {number} the largest number
  */
 
+function maximum (num1, num2){
+  if (num1 > num2){
+    return num1;
+  }
+  else {
+    return num2;
+  }
+}
 
 /**
  * Returns true if `n` is even.
  * @param {number} n
  * @return {boolean} the number is even
  */
+
+ function isEven(num){
+  if (num % 2 === 0){
+    return true;
+  }
+  else {
+    return false;
+  }
+ }
 
 
 /**
@@ -121,6 +229,14 @@
  * @return {boolean} the number is odd
  */
 
+ function isOdd(num){
+  if (num % 2 === 0){
+    return false;
+  }
+  else {
+    return true;
+  }
+ }
 
 /**
  * Returns a letter grade.
@@ -134,6 +250,32 @@
  * @return {string} the score represented as a letter grade
  */
 
+ function letterGrade (score, maxscore){
+  var grade;
+  var percentage = score/maxscore;
+  console.log(percentage);
+  switch (true){
+    case (percentage > 0.89 && percentage <=1.0):
+      grade = "A";
+      break;
+    case (percentage >0.79 && percentage <0.90):
+      grade = "B";
+      break;
+    case (percentage >=0.70 && percentage <0.80):
+      grade = "C";
+      break;
+    case (percentage >0.60 && percentage <0.70):
+      grade = "D";
+      break;
+    case (percentage >0 && percentage <=0.59):
+      grade = "F";
+      break;
+    default:
+      console.log("Enter a valid percentage.");
+  }
+  return grade;
+ }
+
 
 /**
  * Checks if a `restaurant` object has a `reviews` property.
@@ -143,6 +285,20 @@
  * @return {object} restaurant
  */
 
+function incrementReviews(obj){
+  if (Object.prototype.hasOwnProperty.call(obj,'reviews')){
+    obj['reviews'] += 1;
+  }
+  else {
+    obj['reviews'] = 1;
+  }
+
+  return obj;
+}
+ 
+
+
+
 
 /**
  * Joins two strings with a space.
@@ -151,6 +307,11 @@
  * @return {string} joined the words joined with a space
  */
 
+function combine (str1, str2){
+  var arr = [str1, str2];
+  var str3 = arr.join(' ');
+  return str3;
+}
 
 /**
  * Returns a circle object with the properties `circumference` and `area`.
@@ -160,3 +321,9 @@
  * @return {object} circle
  */
 
+ function createCircle (radius){
+  var circle = {};
+  circle.area = Math.PI * radius * radius;
+  circle.circumference = Math.PI * 2 * radius;
+  return circle;
+ }
